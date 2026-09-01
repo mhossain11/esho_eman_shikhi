d4403e94a33d8cf2fcb160a7782af4ce9a79e042
Faysal Hossain <munnafh11@gmail.com>
2026-09-01 10:11:24 +0600

    Last update time change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 8  //Last update 14-8-2026
+        versionCode = 8  //Last update 31-8-2026
         versionName = "1.1.1"
 
     }
